befc5ce878a75ac695be57099fdab6d597b4abaf
Quvid <48755156+KimKyuHoi@users.noreply.github.com>
2023-08-01 11:05:35 +0900

    Feat: 결과창 페이지 구현

--- shareit_frontend/src/components/pages/result/Result.jsx
+++ shareit_frontend/src/components/pages/result/Result.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import styles from "./Result.module.css";
-import Maintitle from "../../../assets/MainTitle.png";
 import Printer from "../../../assets/Printer.png";
-import Flags from "../../../assets/Flags.png";
+import frontendTitle from "../../../assets/FrontendTitle.png";
+import frontendPrinter from "../../../assets/FrontendPrinter.png";
+import frontendFlag from "../../../assets/FrontendFlag.png";
 import BackgroundImage from "../../../assets/background.png";
+import snsBtn from "../../../assets/SNSBtn.png";
+
+const DUMMY_DATA = [
+  {
+    id: "1",
+    title: frontendTitle,
+    printer: frontendPrinter,
+    flags: frontendFlag,
+  },
+];
 
 const Result = () => {
   return (
@@ -12,33 +23,22 @@ const Result = () => {
         className={styles.backGroundImg}
         style={{ backgroundImage: `url(${BackgroundImage})` }}
       />
-      <header className={styles.topTitle}>
-        <div className={styles.Subtitle}>
-          <h5 className={styles.subtTitleContent}>당신의 개발자 성향은?</h5>
-        </div>
-        <div className={styles.Maintitle}>
-          <h5 className={styles.MainTitleContent}>프론트엔드 개발자</h5>
-        </div>
-        <div className={styles.titleContentBox}>
-          <div className={styles.titleContent}>
-            눈에 보이는 화면을 개발하는 프론트엔드. 디자이너, PM, 백엔드
-            사이에서 구체적인 요구사항을 표현합니다. 보다 나은 사용자 경험(UX)를
-            위해 개발합니다.
-          </div>
-        </div>
+      <header className={styles.titleBox}>
+        {DUMMY_DATA.map((data) => (
+          <img src={data.title} alt={data.title} className={styles.title} />
+        ))}
       </header>
-      {/* <main className={styles.contentBox}>
-        <div className={styles.printBox}>
-          <img
-            src={Printer}
-            alt="프린터 이미지입니다."
-            className={styles.printer}
-          />
-        </div>
+      <main className={styles.contentBox}>
+        {DUMMY_DATA.map((data) => (
+          <img src={data.printer} alt={data.title} className={styles.printer}/>
+        ))}
         <div className={styles.etcBox}>
-          <img src={Flags} alt="깃발 이미지입니다." className={styles.Flags} />
+          {DUMMY_DATA.map((data) => (
+            <img src={data.flags} alt={data.title} className={styles.flags}/>
+          ))}
+          <img src={snsBtn} alt="공유 이미지입니다." className={styles.sns} />
         </div>
-      </main> */}
+      </main>
     </div>
   );
 };
