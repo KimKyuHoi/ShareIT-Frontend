367f55f003353d322de6ff06d084316e1f66d934
KimKyuHoi <k546kh@gmail.com>
2023-08-03 17:10:43 +0900

    Refactor: 라우터연결, 폴더 위치 변경 및 코드 수정

--- shareit_frontend/src/App.js
+++ shareit_frontend/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Result from "./components/pages/result/Result";
+import QuestionPage from "./components/pages/Question/QuestionPage";
+import Error from "./components/pages/Error/Error"
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/result" element={<Result />} />
+        <Route path="/question" element={<QuestionPage />} />
+        <Route path='*' element={<Error />} />
       </Routes>
-
     </div>
   );
 }
---
+++ shareit_frontend/src/components/pages/Error/Error.jsx
@@ -0,0 +1,5 @@
+const Error = () =>{
+    return (<div>404에러 페이지입니다.</div>)
+}
+
+export default Error;
